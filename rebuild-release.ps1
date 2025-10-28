c7d81b1d6e5f8d6a71f458c71fa5862e61a4b0df
fgwsz <3191000458@qq.com>
2025-10-28 11:35:10 +0800

    删除无用的构建脚本

--- rebuild-release.ps1
+++
@@ -1,3 +0,0 @@
-$root_path=Split-Path -Parent $MyInvocation.MyCommand.Definition
-&"$root_path/clean-release.ps1"
-&"$root_path/build-release.ps1"
--- rebuild-release.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-root_path=$(dirname "$(readlink -f "$0")")
-
-cd "$root_path"
-
-./clean-release.sh
-./build-release.sh
